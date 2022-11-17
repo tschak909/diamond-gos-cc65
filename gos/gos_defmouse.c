1461e510cb4db7f9c034366382fe02112f55da8f
Thomas <thom.cherryhomes@gmail.com>
2022-11-17 13:03:36 -0600

    add defmouse.

---
+++ gos/gos_defmouse.c
@@ -0,0 +1,19 @@
+/**
+ * Diamond GOS Bindings
+ *
+ * @author  Thomas Cherryhomes
+ * @email   thom dot cherryhomes at gmail dot com
+ * @license gpl v. 3
+ */
+
+#include "gos.h"
+
+/**
+ * @brief Defines a new mouse shape
+ * @param buf pointer to new mouse shape bitmap
+ */
+void gos_defmouse(void *buf)
+{
+  W0=buf;
+  gos(13);
+}

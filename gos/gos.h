3b17db29e9fabae257a277e4bf5a5bc4b115f606
Thomas <thom.cherryhomes@gmail.com>
2022-11-18 12:45:33 -0600

    add messagebox.

--- gos/gos.h
+++ gos/gos.h
@@ -485,3 +485,12 @@ unsigned char gos_fillscreen(unsigned long a, unsigned short p, unsigned char x,
  * @return pointer to filename
  */
 char* gos_fileselect(char *ext, char *def, char *title, char *filename, char *directory);
+
+/**
+ * @brief Creates a simple sub-class of a dialog box, known as a message box. Containing an icon (32x32), a text string, and ewither an OK button or both an OK and cancel button.
+ * @param icon pointer to icon image data
+ * @param s pointer to text string (terminated by 0xFF)
+ * @param t Type of message box (0=OK, 1=OK and Cancel)
+ * @return 0=OK, 1=Cancel
+*/
+unsigned char gos_messagebox(void *icon, char *s, unsigned char t);
